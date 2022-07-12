8dc9295e62c67cbe8c19c03e61cca6b407f82303
puoklam <puoklam0@gmail.com>
2022-07-12 15:07:50 +0000

    cache: correct test case typo

--- cache/lru_test.go
+++ cache/lru_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestEvict(t *testing.T) {
 	c := NewLRU[int, int](2)
 	c.Put(1, 1)
-	c.Put(1, 2)
+	c.Put(2, 2)
 	c.Put(3, 3)
 	if _, ok := c.Get(1); ok {
 		t.Errorf("Should be evicted")
