ef0f8ce437454ed7301b726384127b4d27c65e0f
puoklam <puoklam0@gmail.com>
2022-07-13 02:29:17 +0000

    cache: lru native approach

--- cache/lru.go
+++ cache/lru.go
@@ -1,70 +1,118 @@
 package cache
 
-import "container/list"
+import (
+	"sync"
+)
 
 type node[K comparable, V any] struct {
-	k K
-	v V
+	prev, next *node[K, V]
+	k          K
+	v          V
 }
 
 type LRUCache[K comparable, V any] struct {
-	c uint32
-	m map[K]*list.Element
-	l *list.List
+	mu   sync.RWMutex
+	cap  int
+	len  int
+	root *node[K, V]
+	m    map[K]*node[K, V]
 }
 
-func NewLRU[K comparable, V any](capacity uint32) *LRUCache[K, V] {
-	if capacity == 0 {
-		panic("invalid capacity")
-	}
-	return &LRUCache[K, V]{
-		c: capacity,
-		m: make(map[K]*list.Element),
-		l: list.New(),
-	}
+func NewLRU[K comparable, V any](capacity int) *LRUCache[K, V] {
+	return new(LRUCache[K, V]).Init(capacity)
 }
 
-func (c *LRUCache[K, V]) remove(key K) {
-	e, ok := c.m[key]
-	if !ok {
+func (c *LRUCache[K, V]) add(n *node[K, V]) *node[K, V] {
+	r := c.root
+	n.prev = r
+	n.next = r.next
+	n.prev.next = n
+	n.next.prev = n
+	c.len++
+	return n
+}
+
+func (c *LRUCache[K, V]) addPair(key K, val V) *node[K, V] {
+	return c.add(&node[K, V]{
+		k: key,
+		v: val,
+	})
+}
+
+func (c *LRUCache[K, V]) evict() *node[K, V] {
+	r := c.root
+	l := r.prev
+	l.prev.next = l.next
+	l.next.prev = l.prev
+	if l != r {
+		c.len--
+		l.prev, l.next = nil, nil
+	}
+	return l
+}
+
+func (c *LRUCache[K, V]) refresh(n *node[K, V]) {
+	r := c.root
+	if n == r.next {
 		return
 	}
-	c.l.Remove(e)
-	delete(c.m, key)
-
+	n.prev.next = n.next
+	n.next.prev = n.prev
+	n.prev = r
+	n.next = r.next
+	n.prev.next = n
+	n.next.prev = n
 }
 
-func (c *LRUCache[K, V]) evict() {
-	n := c.l.Remove(c.l.Back()).(*node[K, V])
-	delete(c.m, n.k)
+func (c *LRUCache[K, V]) Init(capacity int) *LRUCache[K, V] {
+	if capacity < 1 {
+		panic("invalid capacity")
+	}
+	c.cap = capacity
+	c.len = 0
+	c.m = make(map[K]*node[K, V])
+	c.root = &node[K, V]{}
+	c.root.prev = c.root
+	c.root.next = c.root
+	return c
 }
 
 func (c *LRUCache[K, V]) Get(key K) (V, bool) {
-	e, ok := c.m[key]
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	n, ok := c.m[key]
 	if !ok {
 		var v V
 		return v, false
 	}
-	c.l.MoveToFront(e)
-	return e.Value.(*node[K, V]).v, true
+	c.refresh(n)
+	return n.v, true
 }
 
 func (c *LRUCache[K, V]) Put(key K, val V) {
-	c.remove(key)
-	if uint32(len(c.m)) == c.c {
-		c.evict()
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if n, ok := c.m[key]; ok {
+		c.refresh(n)
+		n.v = val
+		return
 	}
-	e := c.l.PushFront(&node[K, V]{
-		k: key,
-		v: val,
-	})
-	c.m[key] = e
+	if c.len == c.cap {
+		l := c.evict()
+		delete(c.m, l.k)
+	}
+	n := c.addPair(key, val)
+	c.m[key] = n
 }
 
-func (c *LRUCache[_, _]) Cap() uint32 {
-	return c.c
+func (c *LRUCache[_, _]) Cap() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.cap
 }
 
 func (c *LRUCache[_, _]) Len() int {
-	return c.l.Len()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.len
 }
